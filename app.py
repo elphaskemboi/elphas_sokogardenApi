3de7fa545b176ae92893781838a52d1aeef0ec20
elphas <elphaskemboi52@gmail.com>
2026-02-18 14:01:20 +0300

    project

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import*
+import pymysql
+
+app=Flask(__name__)
+
+@app.route("/api/signup", methods=["POST"])
+def signUp():
+    username= request.form['username']
+    email= request.form['email']
+    phone= request.form['phone']
+    password= request.form['password']
+    
+    print(username,email,phone,password)
+    
+    
+    # create connection to db
+    connection=pymysql.connect(host="localhost",user="root",password="",database="elphas_sokogarden")
+    # create cursor to handle sql queries
+    cursor= connection.cursor()
+    # create sql query
+    sql="insert into users(username,email,phone,password) values(%s,%s,%s,%s)"
+    
+    # data to be saved
+    data= (username,email,phone,password)
+    
+    # execute sql query
+    cursor.execute(sql,data)
+    
+    # save the data
+    connection.commit()
+    # return the response
+    return jsonify({"message":"Sign up succesfull"})
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
