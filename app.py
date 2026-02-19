ae6ee00deaae17ef39a6b60397babe64517fd1f6
elphas <elphaskemboi52@gmail.com>
2026-02-19 10:21:45 +0300

    PROJECT

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import*
 import pymysql
+import pymysql.cursors
 
 app=Flask(__name__)
 
@@ -16,7 +17,11 @@ def signUp():
     # create connection to db
     connection=pymysql.connect(host="localhost",user="root",password="",database="elphas_sokogarden")
     # create cursor to handle sql queries
+    
     cursor= connection.cursor()
+    
+
+    
     # create sql query
     sql="insert into users(username,email,phone,password) values(%s,%s,%s,%s)"
     
@@ -31,8 +36,31 @@ def signUp():
     # return the response
     return jsonify({"message":"Sign up succesfull"})
 
-
-
+# login route
+@app.route("/api/login",methods=["POST"]   )
+def login():
+    email=request.form['email']
+    password=request.form['password']
+    print(email,password)
+    # connect to database
+    connection=pymysql.connect(host="localhost",user="root",password="",database="elphas_sokogarden")
+    # cursor=connection.cursor()
+    # cursor that returns as key-values pair
+    cursor=connection.cursor(pymysql.cursors.DictCursor)
+    # create an sql query
+    sql="select user_id,username,email,phone from users where email=%s and password=%s"
+    # data to execute the query
+    data=(email,password)
+    # execute the query
+    cursor.execute(sql,data)
+    # check for the response
+    if cursor.rowcount==0:
+        return jsonify({"Message":"Invalid credentials"})
+    else:
+        # get the userdata
+        user= cursor.fetchone()
+        return jsonify({"Message":"login succesfull", "user":user})
+    
 
 if __name__=="__main__":
     app.run(debug=True)
